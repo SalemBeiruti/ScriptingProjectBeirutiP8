9e3ddc2acb49473650f7886a85b2a18faf6250e7
Salem Beiruti <726709@student.dusd.net>
2025-01-13 14:05:48 -0800

    enablling
     check state for tuyrning off lights

---
+++ ScriptingProjectP8Beiruti/Assets/Scripts/CheckState.cs
@@ -0,0 +1,21 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckState : MonoBehaviour
+{
+    public GameObject myObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Active SSelf: " + myObject.activeSelf);
+        Debug.Log("Active in Hierarchy" + myObject.activeInHierarchy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
